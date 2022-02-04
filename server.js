5d6233b12ff746c8b9caa4d15b7ee89824708e30
Kurse <lucas.kurse@hotmail.com>
2022-02-04 10:31:02 -0300

    English texts updated

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const next = require("next");
+const path = require("path");
+const fs = require("fs");
+const https = require("https");
+const http = require("http");
+require("dotenv").config();
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  // Projects
+  // server.use("/os-experiment", express.static(path.join(__dirname, "public/os-experiment")));
+
+  server.all("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+
+  /*  if (process.env.NODE_ENV === "development") {
+    server.listen(port, (err) => {
+      if (err) throw err;
+      console.log(`> Ready on http://localhost:${port}`);
+    });
+  } else {
+    const httpsServer = https.createServer(
+      {
+        key: fs.readFileSync(process.env.SSL_KEY),
+        cert: fs.readFileSync(process.env.SSL_CERT),
+      },
+      server
+    );
+
+    httpsServer.listen(443, () => {
+      console.log("HTTPS Server running on port 443");
+    });
+
+    http
+      .createServer(function (req, res) {
+        res.writeHead(301, {
+          Location: "https://" + req.headers["host"] + req.url,
+        });
+        res.end();
+      })
+      .listen(80);
+  } */
+});
