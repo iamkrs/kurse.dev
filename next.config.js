42cf8b4bdbe0bccafe055c03b33337aecefc2660
Kurse <lucas.kurse@hotmail.com>
2024-02-27 13:24:22 -0300

    feat: public pages

--- next.config.js
+++ next.config.js
@@ -7,6 +7,18 @@ const urlRewrites = require("./next.url-rewrites.config");
 module.exports = withNextIntl({
   reactStrictMode: true,
   async rewrites() {
-    return urlRewrites;
+    const fs = require('fs');
+    const path = require('path');
+    const publicPagesPath = path.join(process.cwd(), 'public/p');
+    const publicPagesFolders = fs
+      .readdirSync(publicPagesPath)
+      .filter((file) =>
+        fs.statSync(path.join(publicPagesPath, file)).isDirectory()
+      );
+    const publicPagesIndexes = publicPagesFolders.map((folder) => ({
+      source: `/p/${folder}`,
+      destination: `/p/${folder}/index.html`,
+    }));
+    return [...urlRewrites, ...publicPagesIndexes];
   },
 });
